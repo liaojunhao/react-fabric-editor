17432e57370edb58edd792ae96ca4de085c4596b
liaojunhao <460200123@qq.com>
2023-06-27 20:38:30 +0800

    撤销图标

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -1,10 +1,19 @@
 import { Tooltip, Button } from 'antd';
+import { UndoOutlined, RedoOutlined } from '@ant-design/icons';
 const History = () => {
   return (
     <div style={{ display: 'inline-block' }}>
-      {/* 后退 */}
-      <Tooltip placement="bottom">
-        <Button>后退</Button>
+      {/* 撤销 */}
+      <Tooltip placement="bottom" title="撤销">
+        <Button type="text" size="small" disabled>
+          <UndoOutlined style={{ fontSize: 18 }} />
+        </Button>
+      </Tooltip>
+      {/* 重做 */}
+      <Tooltip placement="bottom" title="重做">
+        <Button type="text" size="small" disabled>
+          <RedoOutlined style={{ fontSize: 18 }} />
+        </Button>
       </Tooltip>
     </div>
   );
