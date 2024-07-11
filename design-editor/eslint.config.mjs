482ec00f5a34f88bbe331d9c49548f033031bf7c
liaojunhao <460200123@qq.com>
2024-07-11 10:50:50 +0800

    save

--- design-editor/eslint.config.mjs
+++ design-editor/eslint.config.mjs
@@ -11,8 +11,12 @@ export default [
   ...tseslint.configs.recommended,
   pluginReactConfig,
   {
+    // 0 关闭
+    // 1 警告
+    // 2 报错
     rules: {
-      'max-len': [1, 130],
+      'max-len': [1, 130], // 长过130就给予警告
+      eqeqeq: 2, // 必须用三等于号做比较
     },
   },
 ];
