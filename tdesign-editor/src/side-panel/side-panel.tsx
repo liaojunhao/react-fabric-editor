3df8b4658520f70b48fc2cd23e328bb0abf14b68
liaojunhao <460200123@qq.com>
2024-07-12 11:56:14 +0800

    panel

--- tdesign-editor/src/side-panel/side-panel.tsx
+++ tdesign-editor/src/side-panel/side-panel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 
 const SidePanelContainer = styled.div`
@@ -7,8 +7,43 @@ const SidePanelContainer = styled.div`
   padding: 0px !important;
   position: relative;
 `;
+
+const TabsWrap = styled.div`
+  @media screen and (min-width: 501px) {
+    overflow-y: auto;
+    overflow-x: hidden;
+    min-width: 72px;
+  }
+`;
+
+const TabsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const PanelContainer = styled.div`
+  padding: 10px 10px 0px 10px !important;
+  height: 100% !important;
+
+  &.bp5-navbar {
+    width: 350px;
+  }
+`;
+
 export const SidePanel = (props) => {
-  return <SidePanelContainer>侧边栏</SidePanelContainer>;
+  const tabsRef = useRef(null);
+  return (
+    <SidePanelContainer className="bp5-navbar tdesign-side-panel">
+      <TabsWrap ref={tabsRef} className="tdesign-side-tabs-container">
+        <TabsContainer className="tdesign-side-tabs-inner">
+          {[1, 2, 3, 4].map((item, index) => {
+            return <div key={index}>{index}</div>;
+          })}
+        </TabsContainer>
+      </TabsWrap>
+      <PanelContainer className="bp5-navbar tdesign-panel-container">内容</PanelContainer>
+    </SidePanelContainer>
+  );
 };
 
 export default SidePanel;
